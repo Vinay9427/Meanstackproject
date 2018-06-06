91ed97de4e9797d73d418bc5c3287e682e6b99e3
Vinay9427 <dupativinay.94@gmail.com>
2018-06-06 12:11:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
                 <div className="row">
                     <div className="col-sm-6 col-md-4">
                         <div className="container">
-                            <div className="jumbotron text-center">
+                            <div className="text-center">
                                 <h1><span className="fa"></span> MiePick</h1> 
                                 <p>Login or Register with:</p> 
                                 <a href="/user/signin" className="btn btn-default"><span className="fa fa-user"></span>  Login</a>
